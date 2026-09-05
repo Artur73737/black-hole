08860473eefc7e3d19869dec61a591a42e52cc36
gabibbo <83822636+Artur73737@users.noreply.github.com>
2026-09-05 10:37:08 +0200

    add deploy page

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/realistic-3d-black-hole-simulation/",
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
