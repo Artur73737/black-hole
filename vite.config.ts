4a13fe54fccb824d37958ca0ca174a3bd33aef90
gabibbo <83822636+Artur73737@users.noreply.github.com>
2026-09-05 10:39:40 +0200

    add asset photo

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/realistic-3d-black-hole-simulation/",
+  base: "/black-hole/",
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
